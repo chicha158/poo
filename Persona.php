8f60029e6b3847340ce67c41025c9c7a6ea726ce
Rafi <ralfsenes@gmail.com>
2017-08-31 17:09:20 -0300

    superclase

--- Persona.php
+++ Persona.php
@@ -6,16 +6,33 @@ class Persona {
     public function Hablar (){
 	    echo " soy ".$this->nombre . " Y esto es Jackass";
 	}
+    public function Acariciar ($perrito) {
+		$perrito->moverCola();
+	}
+}
+
+class Perro {
+    public $nombre;
+    public function moverCola (){
+		echo "muevo la cola";
+	}
 }
 $p = new Persona();
-$p ->nombre = "Micaela";
+$p ->nombre = "Mica";
 $p ->apellido = "Valdez";
 
 $p1 = new Persona();
 $p1 ->nombre = "Juan";
-$p1 ->apellido = "Ylasabichuelas";
+$p1 ->apellido = "Chimichanga";
 
 echo "Hola ".$p->nombre . "<br>";
 echo "Hola ".$p1->nombre . "<br>";
 
 $p->Hablar ();
+
+echo "<br>";
+$perro = new Perro();
+$perro->moverCola();
+echo "<br>";
+
+$p->Acariciar($perro);
